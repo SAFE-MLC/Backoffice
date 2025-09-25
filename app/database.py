b60e99b34ad386710d53eb3c08724ed9dc55ed3a
Luis Felipe <luisfurquijo38@gmail.com>
2025-09-25 14:38:10 -0500

    Add warmup redis db settings

--- app/database.py
+++ app/database.py
@@ -1,7 +1,10 @@
+# app/db.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
-from config import DATABASE_URL
+import redis
+from config import DATABASE_URL, REDIS_URL
 
+# SQLAlchemy
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -12,3 +15,14 @@ def get_db():
     finally:
         db.close()
 
+redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+
+def check_redis():
+    """
+    Comprueba si Redis está conectado.
+    """
+    try:
+        redis_client.ping()
+        print("✅ Redis conectado correctamente")
+    except Exception as e:
+        print("❌ Error al conectar Redis:", e)
