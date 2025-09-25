7988275e39f32e4bcf0ae228c998be6d7738f89c
Luis Felipe <luisfurquijo38@gmail.com>
2025-09-25 14:40:18 -0500

    Router for warmup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routers import staff, tickets
+from app.routers import staff, tickets, warmup
 
 app = FastAPI()
 
 app.include_router(staff.router)
 app.include_router(tickets.router)
+app.include_router(warmup.router)
